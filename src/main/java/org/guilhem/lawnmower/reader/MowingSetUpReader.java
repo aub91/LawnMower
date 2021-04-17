285be5d579cfbb80b4a4c48a93cd4a6d7e7a0aad
aub91 <aubin.guilhem@gmail.com>
2021-04-17 18:46:50 +0200

    Mowing setup can be extract from a file

--- src/main/java/org/guilhem/lawnmower/reader/MowingSetUpReader.java
+++ src/main/java/org/guilhem/lawnmower/reader/MowingSetUpReader.java
@@ -1,9 +1,55 @@
 package org.guilhem.lawnmower.reader;
 
+import org.guilhem.lawnmower.model.Mower;
 import org.guilhem.lawnmower.model.MowingSetUp;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
 public class MowingSetUpReader {
-    public static MowingSetUp readInstruction(String instructionFilePath) {
-        return new MowingSetUp();
+    private static final Logger LOGGER = Logger.getLogger(MowingSetUpReader.class.getName());
+
+    public static MowingSetUp readInstruction(String instructionFilePath) throws IOException {
+
+        MowingSetUp setUp = new MowingSetUp();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(instructionFilePath), StandardCharsets.UTF_8)){
+            extractFieldSize(setUp, reader.readLine());
+            extractMowers(setUp, reader);
+        } catch (FileNotFoundException e) {
+            LOGGER.severe("Can not find file with instruction");
+            throw e;
+        } catch (IOException e) {
+            LOGGER.severe("Error while reading instruction file");
+            throw e;
+        }
+        return setUp;
+    }
+
+    private static void extractFieldSize(MowingSetUp setUp, String fieldSizeData) {
+        String[] fieldSizeDataSplit = fieldSizeData.split(" ");
+        setUp.setMaxX(Integer.parseInt(fieldSizeDataSplit[0]));
+        setUp.setMaxY(Integer.parseInt(fieldSizeDataSplit[1]));
+
+    }
+
+    private static void extractMowers(MowingSetUp setUp, BufferedReader bufferedReader) throws IOException {
+        String mowerStartSetUp = bufferedReader.readLine();
+        do {
+            String[] startSetUp = mowerStartSetUp.split(" ");
+            char[] instructions = bufferedReader.readLine().toCharArray();
+            Mower mower = new Mower(Integer.parseInt(startSetUp[0]), Integer.parseInt(startSetUp[1]), startSetUp[2], instructions);
+
+            setUp.addMower(mower);
+
+            mowerStartSetUp = bufferedReader.readLine();
+        } while (mowerStartSetUp != null);
+
     }
 }
--- src/test/java/org/guilhem/lawnmower/reader/MowingSetUpReaderTest.java
+++ src/test/java/org/guilhem/lawnmower/reader/MowingSetUpReaderTest.java
@@ -1,9 +1,13 @@
 package org.guilhem.lawnmower.reader;
 
+import org.guilhem.lawnmower.model.Mower;
 import org.guilhem.lawnmower.model.MowingSetUp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -16,12 +20,40 @@ public class MowingSetUpReaderTest {
 
     private static final int EXPECTED_MOWER_LIST_SIZE = 2;
 
+    private static final int EXPECTED_MOWER1_INIT_X = 1;
+
+    private static final int EXPECTED_MOWER1_INIT_Y = 2;
+
+    private static final String EXPECTED_MOWER1_INIT_ORIENTATION = "N";
+
+    private static final char[] EXPECTED_MOWER1_INSTRUCTIONS = {'G', 'A', 'G', 'A', 'G','A','G','A', 'A'};
+
+    private static final int EXPECTED_MOWER2_INIT_X = 3;
+
+    private static final int EXPECTED_MOWER2_INIT_Y = 3;
+
+    private static final String EXPECTED_MOWER2_INIT_ORIENTATION = "E";
+
+    private static final char[] EXPECTED_MOWER2_INSTRUCTIONS = {'A', 'A', 'D', 'A', 'A','D','A','D', 'D', 'A'};
+
     @Test
-    public void shouldExtractMowingCondition() {
+    public void shouldExtractMowingCondition() throws IOException {
         MowingSetUp setUp = MowingSetUpReader.readInstruction(INSTRUCTION_PATH);
 
         assertEquals(EXPECTED_MAX_X, setUp.getMaxX());
         assertEquals(EXPECTED_MAX_Y, setUp.getMaxY());
         assertEquals(EXPECTED_MOWER_LIST_SIZE, setUp.getMowers().size());
+
+        Mower mower1 = setUp.getMowers().get(0);
+        assertEquals(EXPECTED_MOWER1_INIT_X, mower1.getINITIAL_X());
+        assertEquals(EXPECTED_MOWER1_INIT_Y, mower1.getINITIAL_Y());
+        assertEquals(EXPECTED_MOWER1_INIT_ORIENTATION, mower1.getINITIAL_ORIENTATION());
+        assertArrayEquals(EXPECTED_MOWER1_INSTRUCTIONS, mower1.getINSTRUCTIONS());
+
+        Mower mower2 = setUp.getMowers().get(1);
+        assertEquals(EXPECTED_MOWER2_INIT_X, mower2.getINITIAL_X());
+        assertEquals(EXPECTED_MOWER2_INIT_Y, mower2.getINITIAL_Y());
+        assertEquals(EXPECTED_MOWER2_INIT_ORIENTATION, mower2.getINITIAL_ORIENTATION());
+        assertArrayEquals(EXPECTED_MOWER2_INSTRUCTIONS, mower2.getINSTRUCTIONS());
     }
 }
